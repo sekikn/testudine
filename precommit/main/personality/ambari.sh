65f43c7d2b103798a100d27b57112852e343f7d7
Allen Wittenauer <aw@apache.org>
2015-06-03 09:11:15 -0700

    ambari oob support

---
+++ precommit/main/personality/ambari.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PATCH_BRANCH_DEFAULT=trunk
+ISSUE_RE='^AMBARI-[0-9]+$'
+HOW_TO_CONTRIBUTE="https://cwiki.apache.org/confluence/display/AMBARI/How+to+Contribute"
+
+function personality_modules
+{
+  local repostatus=$1
+  local testtype=$2
+  local extra=""
+  local fn
+  local i
+
+  testudine_debug "Personality: ${repostatus} ${testtype}"
+
+  clear_personality_queue
+
+  case ${testtype} in
+    javadoc)
+    ;;
+    mvninstall)
+      extra="-DskipTests"
+      if [[ ${repostatus} == branch ]]; then
+        personality_enqueue_module . "${extra}"
+        return
+      fi
+      # mvn install breaks in lots of modules for a variety of reasons
+      # if you do them per-module.  So just force it to be all of them.
+      # personality_enqueue_module . "-DskipTests"
+      return
+      ;;
+    releaseaudit)
+      # this is very fast and provides the full path if we do it from
+      # the root of the source
+      personality_enqueue_module .
+      return
+    ;;
+    unit)
+      if [[ ${TEST_PARALLEL} == "true" ]] ; then
+        extra="-Pparallel-tests"
+        if [[ -n ${TEST_THREADS:-} ]]; then
+          extra="${extra} -DtestsThreadCount=${TEST_THREADS}"
+        fi
+      fi
+    ;;
+    *)
+      extra="-DskipTests"
+    ;;
+  esac
+
+  for module in ${CHANGED_MODULES}; do
+    # shellcheck disable=SC2086
+    personality_enqueue_module ${module} ${extra}
+  done
+}
